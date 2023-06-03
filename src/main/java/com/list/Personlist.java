594b67a195679a52bc364060547d708ce791132f
unknown <singh.prakash816@gmail.com>
2023-06-03 13:32:40 +0530

    message

---
+++ src/main/java/com/list/Personlist.java
@@ -0,0 +1,39 @@
+package com.list;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Personlist {
+	@Id
+	private int id;
+	private String name;
+	private String address;
+	private long phnno;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public long getPhnno() {
+		return phnno;
+	}
+	public void setPhnno(long phnno) {
+		this.phnno = phnno;
+	}
+	
+
+}
